54c6026d4f6e6d9b140d863e930900e508190958
DCB FOSS <86591585+dcbfoss@users.noreply.github.com>
2021-07-06 14:24:54 +0530

    Create bioperl_test.pl

---
+++ test/bioperl_test.pl
@@ -0,0 +1,11 @@
+# sudo cpan install Bio::Perl
+# sudo apt-get install bioperl
+# sudo apt-get install libstring-random-perl
+use Bio::Seq;
+use Bio::Perl;
+use Bio::SeqIO;
+use Bio::DB::GenBank;
+use strict;
+use warnings;
+use String::Random;
+print("Everything is OK\n");
